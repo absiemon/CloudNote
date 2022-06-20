3ffe602f03b98cf83155fc152f5d92be7a5d9fee
Abhay <abc@gmail.com>
2022-06-20 12:09:37 +0530

    added routers(auth.js, notes.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,9 +7,11 @@ connectToMongo();
 const app = express()
 const port = 2000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+// availabel routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
