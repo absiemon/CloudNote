f8f11b469dfd5692e9e9e4a25f3a4b0d6acdd4a9
Abhay <abc@gmail.com>
2022-06-22 12:46:33 +0530

    added fetchallnotes and addnote route in the notes.js

--- BackEnd/models/Notes.js
+++ BackEnd/models/Notes.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
-
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
-    
+    // we also want to note taht which user is adding the notes so that we could put 
+    // a constraint ki koi other user kisi other user ka notes fetch na kar sake. so somehow we have to relate user.js to the notes.js(like foreign key)
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    },
     title:{
         type: String,
         required: true
@@ -21,4 +26,4 @@ const NotesSchema = new Schema({
     },
 });
 
-module.exports = mongoose.model('notes', NotesSchema);  
+module.exports = mongoose.model('notes', NotesSchema);   // notes wii be the name of your collection in the database
--- BackEnd/routes/notes.js
+++ BackEnd/routes/notes.js
@@ -1,9 +1,56 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
+const { body, validationResult } = require('express-validator');  // express validator use to validate user details that he want to send to the database.
 
-    
+const Notes  = require('../models/Notes');
+var fetchuser = require('../middleware/fetchUser');
+
+// ROUTE-1: get all the notes of logged in user using get req on api/notes/fetchallnotes.
+
+router.get('/fetchallnotes', fetchuser, async (req, res)=>{
+
+    try {
+        
+        //fetching all the notes from the database
+        const notes = await Notes.find({user: req.user.id});  // req.user.id will be given by fetchuser middleware
+        res.json(notes); 
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("some error occured");
+    }
+
 })
 
-module.exports = router
+// ROUTE-2: To save the notes of logged in user using POST req on api/notes/addnote .
+// we will use express validator to validate that the user has entered all the fields while adding the notes.
+
+router.post('/addnote', fetchuser, [
+    body('title', 'Please enter the valid title').isLength({min: 3}),
+    body('description', 'Description must be at least 5 characters').isLength({ min: 5}),
+
+], async (req, res)=>{
+
+    try {
+     // checking if any error occured then send the error in json object. error json has msg prop.
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        const {title, description, tag} = req.body;
+
+        const note = new Notes({         // making a new note of type NOtes schema and returning a promise
+            title, description, tag, user : req.user.id 
+
+        })
+        const savedNote = await note.save();  // wait for the user to save the note
+        res.json(savedNote); 
+   
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("some error occured");
+    }
+
+})
+module.exports = router;   
