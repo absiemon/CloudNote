94070bcdb191cadf5d9663324084cc97916566d4
Abhay <abc@gmail.com>
2022-06-22 21:36:47 +0530

    added all the CRUD operatins on notes

--- BackEnd/routes/notes.js
+++ BackEnd/routes/notes.js
@@ -18,7 +18,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res)=>{
 
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("some error occured");
+        res.status(500).send("Internal Server error try after some time ");
     }
 
 })
@@ -49,8 +49,72 @@ router.post('/addnote', fetchuser, [
    
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("some error occured");
+        res.status(500).send("Internal Server error try after some time ");
     }
 
+})
+
+// ROUTE-3: Update the notes of logged in user using PUT req on api/notes/update .
+
+router.put('/update/:id', fetchuser, async (req, res)=>{
+    
+    try{
+        
+        const {title, description, tag} = req.body;
+    
+        // create a new note object 
+        const newNote = {};
+        // title, description, tag is availabel then append to the newNote
+        if(title){ newNote.title = title};
+        if(description){ newNote.description = description};
+        if(tag){ newNote.tag = tag};
+
+        // find the note and update it
+        let note =  await Notes.findById(req.params.id);    // whatever the id is comming from the params , that you have to find
+
+        // if the note that has to be updated is not exiests
+        if(!note){
+           return res.status(404).send({ message: "Note not found" });
+        }
+        
+        // check whether the same user is updating the note 
+        // whenver user creating the note user id bhi note main append ho jaa rahi hain. means note has the userid
+        if(note.user.toString()  !== req.user.id) {          // fetchuser will give req.user.id 
+            return res.status(401).send("Unauthorized access");
+        }
+    
+        // if everythig is fine then update the note
+        note = await Notes.findByIdAndUpdate( req.params.id, {$set: newNote}, {new: true});
+        res.json(note);
+
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal Server error try after some time ");
+    }
+
+})
+
+// ROUTE-3: Delete the notes of logged in user using DELETE req on api/notes/delete .
+router.delete('/delete/:id', fetchuser, async (req, res)=>{
+
+    try{
+        //check whether the note is availabe with given id or not
+        let note = await Notes.findById(req.params.id);
+        if(!note){
+            return res.status(404).send("Notes not found");
+        }
+        // we need to verify that the user that is deleting the note is same as the logged in user
+
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Unauthorized action");
+        }
+        // find and delete the note 
+        note = await Notes.findByIdAndDelete( req.params.id);
+        res.json({"success":"note has been deleted"});
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal Server error occured try after some time ");
+    }
 })
 module.exports = router;   
