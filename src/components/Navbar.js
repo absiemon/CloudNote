ac01e0eb0ce53a96fa28a3a87859363ead8d3961
Abhay <abc@gmail.com>
2022-07-02 21:19:25 +0530

    Final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,9 +16,9 @@ const Navbar = ()=>{
 
     }, [location])
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">iNotebook</Link>
+          <Link className="navbar-brand" to="/">CloudNote</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
